12ca6b41514e46257e085699a980e4dfa781d781
Sangyeop-Lim <dladla5675@gmail.com>
2025-05-09 09:38:36 +0900

    refactor : 임시 주석처리
    임시 주석처리

--- LightAndDark/Assets/SangYupScripts/SangYupManager/StageManager.cs
+++ LightAndDark/Assets/SangYupScripts/SangYupManager/StageManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static StageManager Instance { get; private set; }
+
+    [SerializeField] private int currentStageIndex;
+    //[SerializeField] private List<StageData> stageList;
+
+    private void Awake()
     {
-        
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private void Start()
+    //{
+    //    LoadStage(currentStageIndex);
+    //}
+
+    //public void LoadStage(int index)
+    //{
+    //    if (index < 0 || index >= stageList.Count)
+    //    {
+    //        Debug.LogError("잘못된 스테이지 인덱스");
+    //        return;
+    //    }
+    //    currentStageIndex = index;
+
+    //    Debug.Log($"스테이지 {index} 시작");
+    //}
+
+    //public void UnlockNextStage()
+    //{
+    //    if (currentStageIndex + 1 < stageList.Count)
+    //    {
+    //        currentStageIndex++;
+    //        LoadStage(currentStageIndex);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("모든 스테이지 클리어");
+    //        GameManager.Instance.SetGameState(GameState.StageClear);
+    //    }
+    //}
 }
