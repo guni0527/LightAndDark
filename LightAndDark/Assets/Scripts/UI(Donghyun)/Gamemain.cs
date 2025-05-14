331b78e716a75ed21b8024af05dd006c102ded00
Hyun149 <rusia4527@Gmail.com>
2025-05-14 21:22:33 +0900

    feat(ui): Gammain 메뉴 시스템 마우스 오버 선택 기능으로 구조 개선
    - SelectImages 배열에 EventTrigger 추가하여 PointerEnter 이벤트 등록
      - 마우스 오버 시 HandleMouseEnter() 호출로 SelectNum 자동 변경
    - 기존 SelectBtn() 내부 로직 → Start()에서 초기화 및 키보드 입력만 처리하도록 분리
    - HandleMouseEnter() 메서드 도입하여 OnMouseEnter 이름 충돌 제거 및 명확화
    - UpdateSelectionVisual() 분리하여 선택 이미지 업데이트 일원화
    - 기존 Update()에서 SelectBtn() 호출 유지 (마우스 입력만 지원)
    
    참고: Unity 예약 메서드 네이밍 충돌 방지를 위해 HandleMouseEnter()로 변경

--- LightAndDark/Assets/Scripts/UI(Donghyun)/Gamemain.cs
+++ LightAndDark/Assets/Scripts/UI(Donghyun)/Gamemain.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Gamemain : MonoBehaviour
@@ -13,35 +15,39 @@ public class Gamemain : MonoBehaviour
     private Image cursurImage;
 
 
-    private void SelectBtn()
+    private void Start()
     {
-        if (SelectNum > 0)
+        for (int i = 0; i < SelectImages.Length; i++)
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            int index = i;
+
+            EventTrigger trigger = SelectImages[i].gameObject.GetComponent<EventTrigger>();
+            if (trigger == null)
             {
-                SelectNum--;
+                trigger = SelectImages[i].gameObject.AddComponent<EventTrigger>();
             }
+
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerEnter;
+            entry.callback.AddListener((data) => { HandleMouseEnter(index); });
+            trigger.triggers.Add(entry);
         }
-        if (SelectNum < SelectImages.Length - 1)
-        {
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                SelectNum++;
-            }
-        }
+    }
+
+    private void HandleMouseEnter(int index)
+    {
+        SelectNum = index;
+        UpdateSelectionVisual();
+    }
+
+    private void UpdateSelectionVisual()
+    {
         for (int i = 0; i < SelectImages.Length; i++)
         {
             SelectImages[i].color = new Color(1f, 1f, 1f, 0.2f);
-            if (SelectImages[SelectNum])
-            {
-                SelectImages[SelectNum].color = new Color(1f, 1f, 1f, 1f);
-            }
         }
+        SelectImages[SelectNum].color = new Color(1f, 1f, 1f, 1f);
 
         cursurImage.rectTransform.position = new Vector3(cursurImage.rectTransform.position.x, SelectImages[SelectNum].rectTransform.position.y, cursurImage.rectTransform.position.z);
-    }
-    void Update()
-    {
-        SelectBtn();
     }
 }
